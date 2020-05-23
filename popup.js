8cf612d9094b31f203c15c3a8f268754c3009789
Paulina Khew <paulinakhew12345@hotmail.com>
2020-05-23 19:37:16 -0400

    :package: Save lists into chrome extension

--- popup.js
+++ popup.js
@@ -5,11 +5,24 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
+changeColor.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
       {file: "script.js"});
   });
 };
+
+
+let addLabels = document.getElementById('addLabels');
+
+chrome.storage.sync.get('names', function(data) {
+  console.log(data.key);
+});
+
+addLabels.onclick = function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id);
+  });
+}
--- script.js
+++ script.js
@@ -8,12 +8,22 @@ let label_descriptions = []
 let label_colours = []
 
 for (i = 1; i <= length; i++) {
-    label_names.push(document.querySelectorAll("[id^='label-name']")[i].value);
-    label_descriptions.push(document.querySelectorAll("[id^='label-description']")[i].value);
-    label_colours.push(document.querySelectorAll("[id^='label-color']")[i].value);
-    document.getElementsByClassName("js-edit-label-cancel")[i].click();
+  label_names.push(document.querySelectorAll("[id^='label-name']")[i].value);
+  label_descriptions.push(document.querySelectorAll("[id^='label-description']")[i].value);
+  label_colours.push(document.querySelectorAll("[id^='label-color']")[i].value);
+  document.getElementsByClassName("js-edit-label-cancel")[i].click();
 }
 
 console.log(label_names)
 console.log(label_descriptions)
 console.log(label_colours)
+
+chrome.storage.sync.set(
+  {
+    'names': label_names,
+    'descriptions': label_descriptions,
+    'colours': label_colours
+  }, function(){
+    console.log('saved');
+  }
+)
