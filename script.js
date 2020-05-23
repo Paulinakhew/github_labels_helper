01cc63e438cb2f104a127e311b5c2097a7de1b5b
Paulina Khew <paulinakhew12345@hotmail.com>
2020-05-23 16:49:19 -0400

    :construction: Open all labels and grab label names

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var elements = document.getElementsByClassName("js-edit-label");
-var first = elements[0];
-first.click();
+var length = document.getElementsByClassName("js-edit-label").length;
+for (i = 0; i < length; i++) {
+    var edit = document.getElementsByClassName("js-edit-label")[i];
+    edit.click();
+}
+
+var elems = document.querySelectorAll("[id^='label-name']");
+let label_names = []
+
+for (i = 1; i <= length; i++) {
+    label_names.push(elems[i].value);
+}
+
+console.log(label_names);
