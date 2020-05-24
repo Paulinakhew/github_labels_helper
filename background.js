779d35b4c151d2d839f485f63392ff47f5eebda1
Paulina Khew <paulinakhew12345@hotmail.com>
2020-05-24 19:54:23 -0400

    :fire: Remove unnecessary code

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('This is green.');
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
