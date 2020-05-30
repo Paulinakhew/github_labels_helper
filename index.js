ade46b8ab937648d30baa7a86d886fd202cce5c3
Paulina Khew <paulinakhew12345@hotmail.com>
2020-05-29 20:53:52 -0400

    :children_crossing: Add listener for when the storage values change

--- index.js
+++ index.js
@@ -91,3 +91,16 @@ function createFillButton() {
     }
   });
 }
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    console.log('Storage key "%s" in namespace "%s" changes. ' + 'Old value was "%s", new value "%s".',
+      key,
+      namespace,
+      storageChange.oldValue,
+      storageChange.newValue
+    )
+  }
+
+});
